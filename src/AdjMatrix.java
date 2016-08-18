f467077bd9e8acd7f25161d5e6f74a4179e71a4f
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2016-08-19 01:36:53 +1000

    addVertex() & addEdge() implemented (untested)

--- src/AdjMatrix.java
+++ src/AdjMatrix.java
@@ -25,25 +25,79 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     
     @SuppressWarnings("unchecked")
 	public void addVertex(T vertLabel) {
-    	T[] tempVertex = (T[]) new String[vertex.length+1];
+    	int newSize = vertex.length+1;
+    	
+    	//Copy array of vertices to temp array
+    	T[] tempVertex = (T[]) new String[newSize];
     	for(int i=0;i<vertex.length;i++){
     		tempVertex[i]=vertex[i];
     	}
- 
-    	int[][] tempMatrix = new int[vertex.length+1][vertex.length+1];
-    	//Copy matrix to temporary matrix
-    	for(int i=0;i<vertex.length;i++){
-    		for(int j=0; j<vertex.length;j++){
-    			tempMatrix[i][j]=matrix[i][j];
+
+    	//Copy matrix to temp matrix
+    	int[][] tempMatrix = new int[newSize][newSize];
+    	for(int i=0;i<newSize;i++){
+    		for(int j=0; j<newSize;j++){
+    			if(i==vertex.length||j==vertex.length){
+    				//Set added values to 0
+    				tempMatrix[i][j]=0;
+    			}
+    			else{
+    				tempMatrix[i][j]=matrix[i][j];
+    			}
     		}
     	}
+    	
+    	//Add new vertex to array of vertices
     	tempVertex[vertex.length] = vertLabel;
     	
-    	// Implement me!
+    	//Move elements back to original resized arrays
+    	vertex = (T[]) new String[newSize];
+    	matrix = new int[newSize][newSize];
+    	for(int i=0;i<newSize;i++){
+    		vertex[i]=tempVertex[i];
+    	}
+    	for(int i=0;i<newSize;i++){
+    		for(int j=0; j<newSize;j++){
+    			matrix[i][j]=tempMatrix[i][j];
+    		}
+    	}
+    	
     } // end of addVertex()
 	
     public void addEdge(T srcLabel, T tarLabel) {
-        // Implement me!
+    	//both vertices have to exist
+    	//print to system.err if one does not exist
+    	//Set indices if vertex exists
+    	int srcIndex=-1,tarIndex=-1;
+    	for(int i=0; i<vertex.length;i++){
+    		if(vertex[i]==srcLabel){
+    			srcIndex=i;
+    		}
+    		if(vertex[i]==tarLabel){
+    			tarIndex=i;
+    		}
+    	}
+    	if(srcIndex==-1 && tarIndex==-1){
+    		System.err.println("Vertex "+(String)srcLabel+" and "+(String)tarLabel+" does not exist.");
+    		return;
+    	}else if(srcIndex==-1){
+    		System.err.println("Vertex "+(String)srcLabel+" does not exist.");
+    		return;
+    	}else if(tarIndex==-1){
+    		System.err.println("Vertex "+(String)tarLabel+" does not exist.");
+    		return;
+    	}
+
+    	//if edge already exists, do nothing? maybe print out message
+    	if(matrix[srcIndex][tarIndex]==0 && matrix[tarIndex][srcIndex]==0){
+    		matrix[srcIndex][tarIndex] = matrix[tarIndex][srcIndex] = 1;
+    		System.out.println("Edge added!");
+    	}else if(matrix[srcIndex][tarIndex]==1 && matrix[tarIndex][srcIndex]==1){
+    		System.out.println("Edge already exists!");
+    	}else{
+    		//Should never reach here
+    		System.err.println("Something went wrong! (1)");
+    	}
     } // end of addEdge()
 
     public ArrayList<T> neighbours(T vertLabel) {
